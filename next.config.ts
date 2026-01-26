8f66b29b1949504117b371705a0fa63c7f8d7676
Leonardo Raggio <leonardo.raggio@gmail.com>
2026-01-25 22:42:25 -0300

    Add /admin redirect to Strapi Cloud CMS
    Allows client to access content management at /admin

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "https://promising-benefit-793e66c25a.strapiapp.com/admin",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
