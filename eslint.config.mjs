1e5f83a146d54da2ca43fb8b1f03c82d6aac87d6
Leonardo Raggio <leonardo.raggio@gmail.com>
2026-07-26 00:51:00 -0300

    setup eslint-tailwind plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import betterTailwindcss from "eslint-plugin-better-tailwindcss";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      "better-tailwindcss": betterTailwindcss,
+    },
+    settings: {
+      "better-tailwindcss": {
+        entryPoint: "src/app/globals.css",
+        rootFontSize: 16,
+      },
+    },
+    rules: {
+      "better-tailwindcss/enforce-canonical-classes": "error",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
